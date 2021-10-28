03ec39e75e4fc0579df3a9e9efdc6026418a9c81
fabiankastner <kastner.fabian@gmx.at>
2021-10-28 10:01:16 +0200

    download large set images, resnet 50

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-from utils import get_config
 import time
 import re
 import requests
 from tqdm import tqdm
 import pandas as pd
 import os
+import random
 import pickle
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import logging
@@ -18,42 +18,27 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import callbacks
 from tensorflow.keras.models import load_model
 import datetime
+import cv2
+from tensorflow.keras.applications import ResNet50
 
 
-def download_some_data():
-    config = get_config()
-    prefix = config['scryfall']['prefix']
-    NUM_CARDS = 25
-    DELAY = 0.1
-    suffix = 'cards/random'
-    for i in tqdm(range(NUM_CARDS)):
-        response = requests.get('{0}/{1}'.format(prefix, suffix))
-        data = response.json()
-        img_data = requests.get(data['image_uris']['small']).content
-        card_name = data['name']
-        set_name = data['set_name']
-        with open('data/images/{0}.jpg'.format(i), 'wb') as handler:
-            handler.write(img_data)
+def download_set(set_code='ktk'):
+    uri = 'https://api.scryfall.com/cards/search?q=s%3A{0}'.format(set_code)
+    page = requests.get(uri).json()
+    while page['has_more']:
+        next_page = requests.get(page['next_page']).json()
+        page['data'].extend(next_page['data'])
+        page['has_more'] = next_page['has_more']
+
+    for i, card in tqdm(enumerate(page['data'])):
+        card_name = card['name']
+        set_name = card['set_name']
+        image_url = card['image_uris']['large']
+        image_data = requests.get(image_url).content
+        with open('data/images/{0}.jpg'.format(i+1), 'wb') as handler:
+            handler.write(image_data)
         with open('data/images.csv', 'a') as writer:
-            writer.writelines('{0}.jpg;{1};{2}\n'.format(i, card_name, set_name))
-
-
-
-def get_model(n_classes, input_shape=(32, 32, 3)):
-    model = models.Sequential()
-
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-
-    model.add(layers.Flatten())
-    model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(n_classes))
-    
-    return model
-
+            writer.writelines('{0}.jpg;{1};{2}\n'.format(i+1, card_name, set_name))
 
 
 def train_model():
@@ -76,22 +61,36 @@ def train_model():
     data_generator = data_generator.flow_from_dataframe(
         dataframe=images,
         directory='data/images',
-        target_size=(32, 32),
+        target_size=(256, 256),
         batch_size=32,
-        class_mode='sparse')
+        class_mode='categorical')
 
-    model = get_model(len(le.classes_))
-    
+
+    feature_extractor = ResNet50(weights='imagenet', 
+                             input_shape=(256, 256, 3),
+                             include_top=False)
+
+    feature_extractor.trainable = False
+
+    input_ = tf.keras.Input(shape=(256, 256, 3))
+
+    x = feature_extractor(input_, training=False)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    output_ = tf.keras.layers.Dense(len(le.classes_), activation='softmax')(x)
+
+    model = tf.keras.Model(input_, output_)
+
     EPOCHS = 1000
-    BATCH_SIZE = 32
+    BATCH_SIZE = 64
 
-    callbacks_ = [callbacks.EarlyStopping(monitor='val_loss', patience=EPOCHS/10)]
+    callbacks_ = [callbacks.EarlyStopping(monitor='val_loss', patience=EPOCHS/10),
+                  callbacks.TensorBoard(log_dir='.logs')]
 
     model.compile(optimizer='adam',
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        loss='categorical_crossentropy',
         metrics=['accuracy'])
 
-    history = model.fit(data_generator, epochs=EPOCHS, batch_size=BATCH_SIZE, callbacks=callbacks_)
+    model.fit(data_generator, epochs=EPOCHS, batch_size=BATCH_SIZE, callbacks=callbacks_)
     
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     pickle.dump(le, open('data/label_encoders/le_{0}.pickle'.format(timestamp), 'wb'))
@@ -102,15 +101,26 @@ def train_model():
 def test_model():
     model = load_model('data/models/simple-cnn_2021-10-22-00-39-42')
     le = pickle.load(open('data/label_encoders/le_2021-10-22-00-39-42.pickle', 'rb'))
-    a = 5
+    images = pd.read_csv('data/images.csv', sep=';', names=['filename', 'card_name', 'set_name'])
+    
+    _, filename, name, __ = random.sample(list(images.itertuples()), 1)[0]
+    test_image = cv2.imread('data/images/{0}'.format(filename), cv2.IMREAD_COLOR)
+    resized_test_image = cv2.resize(test_image, (32, 32))
+    
+    prediction = model.predict(np.expand_dims(resized_test_image, 0))
+    predicted_label = le.inverse_transform([np.argmax(prediction[0])])[0]
+
+    cv2.imshow(predicted_label, test_image)
+    cv2.waitKey(0) 
+
 
 
 
 
 def main():
-    # download_some_data()
-    # train_model()
-    test_model()
+    # download_set()
+    train_model()
+    # test_model()
 
 
 if __name__=='__main__':
